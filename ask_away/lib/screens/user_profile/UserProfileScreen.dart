cbf10d17fb7cb842b27ec81f3f520696efa45ad2
Educorreia932 <skelozard1@gmail.com>
2020-11-19 23:30:00 +0000

    Added an app bar to the user profile and stylized the background

--- ask_away/lib/screens/user_profile/UserProfileScreen.dart
+++ ask_away/lib/screens/user_profile/UserProfileScreen.dart
@@ -11,88 +11,93 @@ class UserProfileState extends State<UserProfile> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Stack(children: [
-            Container(
-              height: 300.0,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(17.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  CircleAvatar(
-                    backgroundImage: NetworkImage("https://i.pravatar.cc/300"),
-                    radius: 35,
-                  ),
-                  Text(
-                    "John Doe",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 28.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const Divider(
-                    color: Colors.black,
-                    height: 20,
-                    thickness: 1,
-                    indent: 20,
-                    endIndent: 0,
-                  ),
-                ],
-              ),
-            ),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: Text('User Profile'),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: SweepGradient(colors: [
+            Colors.blue,
+            Colors.lightBlue,
+            Colors.teal,
           ]),
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Color.fromRGBO(242, 242, 242, 1.0),
-                  borderRadius: BorderRadius.only(
-                    topRight: Radius.circular(20),
-                    topLeft: Radius.circular(20),
-                  )),
-              child: Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      "Recent Questions",
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 80.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(children: [
+                Padding(
+                  padding: const EdgeInsets.all(17.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      CircleAvatar(
+                        backgroundImage:
+                            NetworkImage("https://i.pravatar.cc/300"),
+                        radius: 35,
                       ),
+                      Text(
+                        "John Doe",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Divider(
+                        color: Colors.lightBlue,
+                        height: 20,
+                        thickness: 1,
+                      ),
+                    ],
+                  ),
+                ),
+              ]),
+              Expanded(
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Color.fromRGBO(242, 242, 242, 1.0),
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                        topLeft: Radius.circular(20),
+                      )),
+                  child: Padding(
+                    padding: const EdgeInsets.all(18.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "Recent Questions",
+                          style: TextStyle(
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Container(
+                          height: 150.0,
+                          child: ListView(
+                            padding: const EdgeInsets.only(right: 48),
+                            scrollDirection: Axis.horizontal,
+                            children: [
+                              QuestionCard(),
+                              QuestionCard(),
+                              QuestionCard()
+                            ],
+                          ),
+                        )
+                      ],
                     ),
-                    Container(
-                      height: 200.0,
-                      child: ListView(
-                        scrollDirection: Axis.horizontal,
-                        children: [
-                          QuestionCard(),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          QuestionCard(),
-                          SizedBox(
-                            width: 20,
-                          ),
-                          QuestionCard()
-                        ],
-                      ),
-                    )
-                  ],
+                  ),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
+        ),
       ),
     );
   }
 }
-
-
--- ask_away/lib/screens/user_profile/components/QuestionCard.dart
+++ ask_away/lib/screens/user_profile/components/QuestionCard.dart
@@ -3,44 +3,46 @@ import 'package:flutter/material.dart';
 class QuestionCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(20))),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "Question #1",
-            style: TextStyle(
-              fontSize: 18.0,
-              fontWeight: FontWeight.w600,
+    return Card(
+      child: Container(
+        width: 260,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(20))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Question #1",
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.w600,
+              ),
             ),
-          ),
-          Row(
-            children: [Text("Jane Doe"), Text("1d ago")],
-          ),
-          RichText(
-            overflow: TextOverflow.ellipsis,
-            maxLines: 2,
-            text: TextSpan(
-                style: TextStyle(color: Colors.black),
-                text:
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae quam eros. Cras interdum lacus sed erat pulvinar placerat. Mauris laoreet purus at neque semper, nec bibendum lacus consectetur."),
-          ),
-          Row(
-            children: [
-              Icon(Icons.keyboard_arrow_up),
-              Text("10"),
-              Icon(Icons.keyboard_arrow_down),
-              Icon(Icons.comment),
-              Text("8"),
-              Icon(Icons.remove_red_eye),
-              Text("50"),
-            ],
-          )
-        ],
+            Row(
+              children: [Text("Jane Doe"), Text("1d ago")],
+            ),
+            RichText(
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+              text: TextSpan(
+                  style: TextStyle(color: Colors.black),
+                  text:
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vitae quam eros. Cras interdum lacus sed erat pulvinar placerat. Mauris laoreet purus at neque semper, nec bibendum lacus consectetur."),
+            ),
+            Row(
+              children: [
+                Icon(Icons.keyboard_arrow_up),
+                Text("10"),
+                Icon(Icons.keyboard_arrow_down),
+                Icon(Icons.comment),
+                Text("8"),
+                Icon(Icons.remove_red_eye),
+                Text("50"),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
