07c9f87a27de40ed7272a57723cb557aea8bd993
Henrique <gamingwithapotato@gmail.com>
2020-12-11 16:39:10 +0000

    Cleaned up some code

--- ask_away/test/app.dart
+++ ask_away/test/app.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_driver/driver_extension.dart';
 import 'package:ask_away/main.dart' as app;
@@ -6,6 +5,5 @@ import 'package:ask_away/main.dart' as app;
 Future<void> main() async {
 // This line enables the extension.
   enableFlutterDriverExtension();
-  await Firebase.initializeApp();
   runApp(app.MyApp());
 }
--- ask_away/test/test_config.dart
+++ ask_away/test/test_config.dart
@@ -1,4 +1,3 @@
-
 import 'dart:async';
 import 'package:flutter_gherkin/flutter_gherkin.dart';
 import 'package:gherkin/gherkin.dart';
